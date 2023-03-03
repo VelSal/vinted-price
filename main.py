506e24f25ec78d217701c8827036478999f08adc
VelSal <v.salvatore@live.be>
2023-03-03 14:30:37 +0100

    Added shipping fees

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 price = input("Prix revendeur: ")
 
 def vinted():
-
   result = (((float(price) * 5) / 100) + 0.7 + float(price))
   return result
-
 print("Prix avec charges vinted (HFDP): " + str(vinted()) + "€")
+
+shipping = input("Frais de port: ")
+def vintedShipping():
+  result = (vinted() + float(shipping))
+  return result
+print("Prix avec frais de port: " + str(vintedShipping()) + "€")
